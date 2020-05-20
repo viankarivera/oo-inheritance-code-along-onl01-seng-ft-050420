3db043c651dcbd6ddc9c929929ddee052e4ffdab
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 18:04:40 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,9 +4,9 @@ class Vehicle
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
-  end 
+  end
 
-  def go 
+  def go
     "vrrrrrrrooom!"
   end 
 
